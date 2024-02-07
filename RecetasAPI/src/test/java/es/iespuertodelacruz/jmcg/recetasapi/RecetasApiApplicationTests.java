5c6a5bb08745ea812bc2ed6860597bf929e81f9a
JavierCedres <javmarcego@gmail.com>
2024-02-07 02:10:15 +0000

    feat():Creada la estructura de ramas del repositorio

---
+++ RecetasAPI/src/test/java/es/iespuertodelacruz/jmcg/recetasapi/RecetasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.jmcg.recetasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
